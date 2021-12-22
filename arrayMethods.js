7ed98f14ddc1104f6285559d62624df7a5e63114
JadenFicklin <jadendficklin@gmail.com>
2021-12-22 13:41:23 -0700

    Completed problem 2

--- arrayMethods.js
+++ arrayMethods.js
@@ -41,7 +41,10 @@ const prices = [15.0, 23.0, 78.0, 34.0, 12.0, 86.0, 12.0, 79.0, 32.0];
 */
 
 // CODE HERE
-// const postTaxPrices // = prices.map(/* Provide Your Callback Here );
+const postTaxPrice = prices.map(function (tax) {
+  return (tax = tax * 1.07);
+});
+console.log(postTaxPrice);
 
 ////////// PROBLEM 3 //////////
 
